56f5deaf3b9024dd2f41876a4aed9df1e43bf08b
Alexis Williams <alexis@typedr.at>
2023-04-15 22:17:05 -0700

    Initial Commit

---
+++ bijutsukan/bijutsukanApp.swift
@@ -0,0 +1,17 @@
+//
+//  bijutsukanApp.swift
+//  bijutsukan
+//
+//  Created by Alexis Williams on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct bijutsukanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
